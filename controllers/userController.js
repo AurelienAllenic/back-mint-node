f90220862c75b29d361cbdd1bb6f71235a3995ca
AurelienAllenic <aurelien.allenic@gmail.com>
2025-07-23 19:13:29 +0200

    (fix) added first and lastnames to get users

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,14 +3,12 @@ const User = require("../models/User");
 // Récupérer tous les utilisateurs (email et id seulement)
 exports.getUsers = async (req, res) => {
   try {
-    const users = await User.find({}, "_id email");
+    const users = await User.find({}, "_id email firstname lastname");
     res.status(200).json(users);
   } catch (error) {
-    res
-      .status(500)
-      .json({
-        message: "Erreur lors de la récupération des utilisateurs.",
-        error,
-      });
+    res.status(500).json({
+      message: "Erreur lors de la récupération des utilisateurs.",
+      error,
+    });
   }
 };
