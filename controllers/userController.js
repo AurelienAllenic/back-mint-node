d735a29fd0533887e45e82c356c845659a3ca609
AurelienAllenic <aurelien.allenic@gmail.com>
2025-07-23 20:09:01 +0200

    (feat) added route tu update first and lastname of user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,29 @@
+// Modifier le prénom et/ou le nom d'un utilisateur
+exports.updateUserNames = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { firstname, lastname } = req.body;
+    if (!firstname && !lastname) {
+      return res.status(400).json({ message: "Aucun champ à mettre à jour." });
+    }
+    const update = {};
+    if (firstname !== undefined) update.firstname = firstname;
+    if (lastname !== undefined) update.lastname = lastname;
+    const user = await User.findByIdAndUpdate(userId, update, {
+      new: true,
+      select: "_id email firstname lastname",
+    });
+    if (!user) {
+      return res.status(404).json({ message: "Utilisateur non trouvé." });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({
+      message: "Erreur lors de la mise à jour de l'utilisateur.",
+      error,
+    });
+  }
+};
 const User = require("../models/User");
 
 // Récupérer tous les utilisateurs (email et id seulement)
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,4 +5,7 @@ const auth = require("../middleware/auth");
 
 router.get("/", auth, userController.getUsers);
 
+// Modifier prénom et/ou nom d'un utilisateur
+router.patch("/:userId/names", auth, userController.updateUserNames);
+
 module.exports = router;
