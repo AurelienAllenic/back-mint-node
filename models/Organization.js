50b3bade03b789e51a77c8e1568ed3cdc7e57e17
enzocosson <enzolemercier@gmail.com>
2025-07-23 21:56:34 +0200

    fix : changes number to string types

--- models/Organization.js
+++ models/Organization.js
@@ -5,8 +5,8 @@ const OrganizationSchema = new mongoose.Schema(
     name: { type: String, required: true },
     media_avatar_id: { type: Number },
     media_banner_id: { type: Number },
-    created_by_id: { type: Number },
-    owner_id: { type: Number },
+    created_by_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    owner_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   },
   { timestamps: true }
 );
