722d668515209b0fc2de3c94a24cfc9eaefd58ac
AurelienAllenic <aurelien.allenic@gmail.com>
2025-07-23 20:17:02 +0200

    (fix) corrected wrong route for visitor token

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,9 @@
-// Route pour obtenir un token visiteur temporaire
-router.post(
-  "/visitor-token",
-  require("../controllers/authController").visitorToken
-);
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
+// Route pour obtenir un token visiteur temporaire
+router.post("/visitor-token", authController.visitorToken);
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/verify-token", authController.verifyToken);
