7439257f95d4c93e42269f27da3da66d91dd1141
AurelienAllenic <aurelien.allenic@gmail.com>
2025-07-24 14:54:20 +0200

    (fix) regave access to visitor with simple url

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 
 // Route pour obtenir un token visiteur temporaire
-//router.post("/visitor-token", authController.visitorToken);
+router.post("/visitor-token", authController.visitorToken);
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.get("/verify-token", authController.verifyToken);
